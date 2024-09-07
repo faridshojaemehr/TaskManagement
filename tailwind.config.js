6a05c5453756f1d3aced4b68759f1a046eac6f23
farid shojaemehr <farid.juve@gmail.com>
2024-09-08 02:10:10 +0300

    implement task-card

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ module.exports = {
     colors: {
       main: "#274E61",
       task_board: "#EAEEF0",
+      task_title: "#707070",
       toolbar: "#768490",
+      white: "#ffffff",
+      task_deatils: "#ABABAB",
+      task_process: "#E1E1E1",
+      red: "red",
     },
   },
   plugins: [require("daisyui")],
